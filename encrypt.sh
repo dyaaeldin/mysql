4e6167e7faa6ffe513b0498bb692a206342b8250
Dyaa Ahmed <79382618+dyaaeldin@users.noreply.github.com>
2023-10-11 06:42:07 +0300

    Create encrypt.sh

---
+++ encrypt.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 <database_name>"
+  exit 1
+fi
+
+DB_NAME="$1"
+
+tables=$(mysql $DB_NAME -e "SHOW TABLES" | tail -n +2)
+
+for table in $tables; do
+  mysql $DB_NAME -e "ALTER TABLE $table ENGINE=InnoDB"
+  mysql $DB_NAME -e "ALTER TABLE $table ENCRYPTION='Y'"
+done
+
+echo "Storage engine and encryption enabled successfully for all tables in database: $DB_NAME"
