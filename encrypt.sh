6f45ee9b161bd4c0f79c70322589191dd252a6c6
Dyaa Ahmed <79382618+dyaaeldin@users.noreply.github.com>
2023-10-11 06:46:17 +0300

    Update encrypt.sh

--- encrypt.sh
+++ encrypt.sh
@@ -15,3 +15,8 @@ for table in $tables; do
 done
 
 echo "Storage engine and encryption enabled successfully for all tables in database: $DB_NAME"
+
+
+### Change default storage engine
+#[mysqld]
+#default-storage-engine = innodb
